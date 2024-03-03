77f5b04033e0163460f1b1b40d317debd5e5293c
Edoardo Grassi <edo.g@hotmail.it>
2024-03-03 22:53:50 +0100

    fix: base url with env

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -9,7 +9,7 @@ declare module 'vue-router' {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: '/',
